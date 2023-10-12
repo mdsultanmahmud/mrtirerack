bc01affdab0afc0d6ff70aef85653c759e245594
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-10-12 16:14:32 +0600

    navbar for large device done

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,9 +1,33 @@
-import React from 'react';
-
+import Link from 'next/link';
+import React, { useState } from 'react';
+import styles from '@/styles/navbar.module.css'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { RxCross1 } from 'react-icons/rx'
 const Navbar = () => {
+    const [search, setSearch] = useState(false)
     return (
-        <div>
-            <h1>This is my Navbar</h1>
+        <div className='shadow-2xl'>
+            <div className='mainContainer'>
+                <nav className={`${styles.navContainer} px-4`}>
+                    <ul>
+                        <Link href={"/"} className='text-2xl uppercase font-bold'>Icon</Link>
+                    </ul>
+                    <ul>
+                        <li><Link href={"tricks"}>Learn & How To</Link></li>
+                        <li><Link href={"review"}>Tire Review</Link></li>
+                        <li><Link href={"best-tire"}>Best Tires</Link></li>
+                        <div>
+                            <input hidden={!search} autoFocus />
+                            {
+                                !search ?
+                                    <AiOutlineSearch size={28} className='cursor-pointer' onClick={() => setSearch(true)} />
+                                    :
+                                    <RxCross1 size={24} className='cursor-pointer' onClick={() => setSearch(false)} />
+                            }
+                        </div>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 };
